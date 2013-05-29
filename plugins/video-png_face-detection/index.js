b0c296ded5127e1e6fe493a64ee5bdc64da28db2
Philmod <philmod@storify.com>
2013-05-28 22:09:04 -0700

    if error detecting object, just save the image as it

--- plugins/video-png_face-detection/index.js
+++ plugins/video-png_face-detection/index.js
@@ -7,7 +7,9 @@ function video(name, deps) {
     
   var latestImage;
 
-  // Add a new route to fetch camera image
+  /**
+   * Add a new route to fetch camera image
+   */
   deps.app.get('/camera/:id', function(req, res) {
     if (!latestImage) {
       res.writeHead(301, {"Location": "/plugin/" + name + "/images/nofeed.jpg"});
@@ -19,18 +21,26 @@ function video(name, deps) {
     return res.end(latestImage, "binary");
   });
 
-  // Stream the images into a stream of Matrices
-  s.on('data', function(matrix){
-    matrix.detectObject(cv.FACE_CASCADE, {}, function(e, faces) {
-      if (e) return console.error('Error while detecting object on image : ', e);
-      for (var i=0; i<faces.length; i++) {
-        var x = faces[i];
-        matrix.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
-      }
-      latestImage = matrix.toBuffer();
-    });
-  })
+  /**
+   * Stream the images into a stream of Matrices
+   */
   deps.client.createPngStream().pipe(s);
+  
+  s.on('data', function(matrix){
+
+    matrix.detectObject(cv.FACE_CASCADE, {}, function(e, faces) {  // Detect object
+      if (e) {
+        console.error('Error while detecting object on image : ', e);
+      } else {
+        for (var i=0; i<faces.length; i++) {  // Loop over the faces
+          var x = faces[i];
+          matrix.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2); // Circle the face
+        }
+      }
+      latestImage = matrix.toBuffer(); // Save the image to latestImage
+    });
+
+  });
 
 };
 
